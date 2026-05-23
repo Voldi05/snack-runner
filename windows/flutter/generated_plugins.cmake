7f0ba5502e6046f4869cda83fa4576cb5eedff19
Voldigod <njoya15@yahoo.com>
2026-05-23 12:09:26 +0100

    fix: remove duplicate supabase_flutter in pubspec.yaml

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,16 +4,12 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
-<<<<<<< HEAD
-  flutter_secure_storage_windows
-=======
   firebase_core
->>>>>>> cd5c9df17e738c784c27ee3f18f7814b8deb1fce
+  flutter_secure_storage_windows
   url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
